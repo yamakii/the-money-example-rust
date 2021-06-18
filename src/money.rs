2a3bfeda693c1a34d444a75a4f8be3f8849631b3
yamaki <yamaki@wh-plus.com>
2021-06-18 22:02:40 +0900

    「仮実装」でtimesを追加

--- src/money.rs
+++ src/money.rs
@@ -1,7 +1,20 @@
-pub struct Money{}
+struct Dollar {
+    amount: i32,
+}
+
+impl Dollar {
+    fn new(amount: i32) -> Self {
+        Dollar { amount }
+    }
+    fn times(&mut self, multiplier: i32) {
+        self.amount *= multiplier;
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::money::Dollar;
+
     #[test]
     fn test_multiplication() {
         let mut five = Dollar::new(5);
