6bfde140d39672726c5fd34e395e70303ca48910
yamaki <yamaki@wh-plus.com>
2021-06-28 00:28:17 +0900

    MoneyにTrait境界を追加

--- src/money.rs
+++ src/money.rs
@@ -1,45 +1,64 @@
 #![allow(dead_code)]
 
+use std::marker::PhantomData;
+
+trait Currency {
+    fn currency() -> &'static str;
+}
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-struct Money {
-    amount: i32,
-    currency: &'static str,
+enum Dollar {}
+
+impl Currency for Dollar {
+    fn currency() -> &'static str { "USD" }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Franc {}
+
+impl Currency for Franc {
+    fn currency() -> &'static str { "CHF" }
 }
 
 
-impl Money {
-    fn new(amount: i32, currency: &'static str) -> Self {
-        Money { amount, currency }
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Money<T: Currency> {
+    amount: i32,
+    currency: PhantomData<T>,
+}
+
+impl<T: Currency> Money<T> {
+    fn new(amount: i32) -> Self {
+        Self { amount, currency: PhantomData::<T> }
     }
-    fn dollar(amount: i32) -> Self { Money::new(amount, "USD") }
-    fn franc(amount: i32) -> Self { Money::new(amount, "CHF") }
     fn times(self, multiplier: i32) -> Self {
-        Money::new(self.amount * multiplier, self.currency)
+        Money::new(self.amount * multiplier)
     }
-    fn currency(self) -> &'static str { self.currency }
+    fn currency(self) -> &'static str {
+        T::currency()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::money::{Money};
+    use crate::money::{Money, Dollar, Franc};
 
     #[test]
     fn test_currency() {
-        assert_eq!("USD", Money::dollar(1).currency());
-        assert_eq!("CHF", Money::franc(1).currency());
+        assert_eq!("USD", Money::<Dollar>::new(1).currency());
+        assert_eq!("CHF", Money::<Franc>::new(1).currency());
     }
 
     #[test]
     fn test_multiplication() {
-        let five = Money::dollar(5);
-        assert_eq!(Money::dollar(10), five.times(2));
-        assert_eq!(Money::dollar(15), five.times(3));
+        let five = Money::<Dollar>::new(5);
+        assert_eq!(Money::<Dollar>::new(10), five.times(2));
+        assert_eq!(Money::<Dollar>::new(15), five.times(3));
     }
 
     #[test]
     fn test_equality() {
-        assert_eq!(Money::dollar(5), Money::dollar(5));
-        assert_ne!(Money::dollar(5), Money::dollar(6));
-        assert_ne!(Money::dollar(5), Money::franc(5));
+        assert_ne!(Money::<Dollar>::new(5), Money::<Dollar>::new(6));
+        assert_eq!(Money::<Dollar>::new(5), Money::<Dollar>::new(5));
     }
 }
