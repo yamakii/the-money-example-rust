6433d29c7a0b6f50d9b4c4132ebcf5d82b5052b4
yamaki <yamaki@wh-plus.com>
2021-06-30 23:46:00 +0900

    MoneyにAddを追加(途中)

--- src/money.rs
+++ src/money.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use std::marker::PhantomData;
+use std::ops::Add;
 
 trait Currency {
     fn currency() -> &'static str;
@@ -18,6 +19,10 @@ enum Franc {}
 
 impl Currency for Franc {
     fn currency() -> &'static str { "CHF" }
+}
+
+enum Expression<T: Currency> {
+    Sum(Sum<T>),
 }
 
 
@@ -39,14 +44,78 @@ impl<T: Currency> Money<T> {
     }
 }
 
+impl<T: Currency> Add for Money<T> {
+    type Output = Expression<T>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Expression::Sum(Sum::new(self, rhs))
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Bank {}
+
+impl Bank {
+    fn reduce<T: Currency>(self, source: Expression<T>) -> Money<T> {
+        match source {
+            Expression::Sum(sum) => sum.reduce(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Sum<T: Currency> {
+    augend: Money<T>,
+    addend: Money<T>,
+}
+
+impl<T: Currency> Sum<T> {
+    fn new(augend: Money<T>, addend: Money<T>) -> Self {
+        Sum { augend, addend }
+    }
+    fn reduce(self) -> Money<T> {
+        Money::new(self.augend.amount + self.addend.amount)
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::money::{Money, Dollar, Franc};
+    use crate::money::{Money, Dollar, Franc, Bank, Sum, Expression};
 
     #[test]
     fn test_currency() {
         assert_eq!("USD", Money::<Dollar>::new(1).currency());
         assert_eq!("CHF", Money::<Franc>::new(1).currency());
+    }
+
+    #[test]
+    fn test_simple_addition() {
+        let five = Money::<Dollar>::new(5);
+        let sum = five + five;
+        let bank = Bank {};
+        let reduced = bank.reduce::<Dollar>(sum);
+        assert_eq!(Money::<Dollar>::new(10), reduced);
+    }
+
+    #[test]
+    fn test_plus_return_sum() {
+        let five = Money::<Dollar>::new(5);
+        let result = five + five;
+        let sum = match result { Expression::Sum(x) => x };
+        assert_eq!(five, sum.augend);
+        assert_eq!(five, sum.addend);
+    }
+
+    #[test]
+    fn test_plus_reduce_sum() {
+        let sum = Expression::Sum(
+            Sum::new(
+                Money::<Dollar>::new(3),
+                Money::<Dollar>::new(4))
+        );
+        let bank = Bank {};
+        let reduced = bank.reduce::<Dollar>(sum);
+        assert_eq!(Money::<Dollar>::new(7), reduced);
     }
 
     #[test]
--- todo.md
+++ todo.md
@@ -1,15 +1,18 @@
-- [ ] $5 + 10 CHF = $10 (レートが2:1)の場合
 - [x] $5 * 2 = $10
 - [x] amountをprivateにする
 - [x] Dollarの副作用をどうする？
-- [ ] Moneyの丸め処理をどうする？
 - [x] equals
 - [x] nullとの等価比較(対象外)
-- [ ] 他のオブジェクトとの等価比較
 - [x] 5CHF * 2 = 10CHF
-- [ ] DollarとFrancの重複
 - [x] equalsの一般化(対象外)
 - [x] timesの一般化
 - [x] FrancとDollarを比較する
 - [x] 通貨の概念
+- [ ] $5 + 10 CHF = $10 (レートが2:1)の場合
+- [ ] $5 + $5 = $10
+- [ ] $5 + $5 がMoneyを返す
+- [ ] Bank.reduce(Money)
+- [ ] Moneyの丸め処理をどうする？
+- [ ] 他のオブジェクトとの等価比較
+- [ ] DollarとFrancの重複
 - [ ] test_franc_multiplicationを削除する？
